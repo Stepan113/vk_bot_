794e8ffc661d31dd95b8b4578d9fad02023db6a4
Ctepa <CtepanCecevitcin@yandex.ru>
2024-01-29 17:17:28 +0300

    Изменил поля бд

--- vk_bot/database/create_database.py
+++ vk_bot/database/create_database.py
@@ -12,6 +12,8 @@ class Base(DeclarativeBase):
 class Question(Base):
     __tablename__ = "questions"
     id: Mapped[int] = mapped_column(primary_key=True)
+    number_question: Mapped[int]
     question: Mapped[str]
     link_on_image: Mapped[str]
     choose_questions: Mapped[str]
+    right_answer: Mapped[str]

c5380a2a9339b88923e843076ab7df937f94442a
Ctepa <CtepanCecevitcin@yandex.ru>
2024-01-29 17:12:03 +0300

    Описал декоратор, пытаюсь придумать логику декоратору

--- vk_bot/bot/main.py
+++ vk_bot/bot/main.py
@@ -9,8 +9,8 @@ longpoll = VkLongPoll(vk)
 
 
 class FSM(object):
-    def __int__(self, state):
-        self.now_state = state
+    def __init__(self):
+        self.now_state = ""
         self.question = 'question'
         self.answer = 'answer'
         self.expectation = 'expectation'
@@ -25,12 +25,37 @@ class FSM(object):
         return self.expectation
 
 
-def writeMessage(message: str, user_id: int):
+def trackingStatusQuestion(question: str, answers: list):
+    message_1 = ''
+    if len(question) > 0 and len(answers) > 0:
+        message_1 = question + '\n'
+        for index, answer in enumerate(answers):
+            message_1 += str(index + 1) + ")" + str(answer) + "\n"
+
+    def func_decorator(func):
+        def trackStatus(message, user_id, *args, **kwargs):
+            if len(message_1) > 0:
+                func(message_1, user_id, *args, **kwargs)
+            else:
+                func(message, user_id, *args, **kwargs)
+
+        return trackStatus
+
+    return func_decorator
+
+
+question: str = ''
+answers: list = []
+
+
+@trackingStatusQuestion(question=question, answers=answers)
+def writeMessage(message, user_id):
     vk.method("messages.send", {
         "user_id": user_id,
         "message": message,
         "random_id": get_random_id(),
     })
+    print("Я написал такое сообщение: ", message)
 
 
 def sendImage(user_id: str, photo: str):
@@ -57,5 +82,6 @@ for event in longpoll.listen():
         #     Что изображено на картинке?"""
         #     fsm.now_state = fsm.questions()
         else:
+            question += "shgakjc"
             message = message_user
             writeMessage(message, user_id)
